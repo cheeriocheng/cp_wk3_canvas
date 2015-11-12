2f289f2bbef343142f290724b68c836bb78ecb9e
cheng <gnuchex@gmail.com>
2015-11-12 15:51:07 -0800

    first commit

--- cp_canvas/mainViewController.swift
+++ cp_canvas/mainViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
+    
 class mainViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func didPanTray(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        let point = panGestureRecognizer.locationInView(view)
+        let translation = panGestureRecognizer.translationInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+                    print("Gesture began at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+                    print("Gesture changed at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+                    print("Gesture ended at: \(point)")
+                }
+
+        
+    }
+    
+    
+//    
+//    @IBAction func didPanTray(panGestureRecognizer: UIPanGestureRecognizer) {
+//        
+//        print("in didPanTray")
+//        
+//        let point = panGestureRecognizer.locationInView(view)
+//        let velocity = panGestureRecognizer.velocityInView(view)
+//        let translation = panGestureRecognizer.translationInView(view)
+//        
+//        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+//            print("Gesture began at: \(point)")
+//        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+//            print("Gesture changed at: \(point)")
+//        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+//            print("Gesture ended at: \(point)")
+//        }
+    
+        
+        
+//    }
+
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
